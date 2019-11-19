550d9e0f3bd7080a7043340b025573150e21ea3a
Terry Wang <terrywangce@gmail.com>
2019-11-19 15:32:52 -0800

    Created servlets, finished login, register, signout

--- src/Register.java
+++ src/Register.java
@@ -11,16 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class Register
  */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
+@WebServlet("/Register")
+public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public Register() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,32 +30,35 @@ public class Login extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String loginError = null;
+		String registerError = null;
 		HttpSession session = request.getSession();
-		System.out.println("print1: "+request.getSession().getId());
-		String forwardUrl = "/login.jsp";
+		String forwardUrl = "/register.jsp";
+		//Get all the parameters
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+		String cPassword = request.getParameter("cPassword");
 		User user = new User(username,password);
 		accessData access = new accessData();
-		int userExist = access.validateUser(user);
+		int userExist = access.registerUser(user, cPassword);
 		if(userExist == 0) {
-			loginError = "This user does not exist";
+			registerError = "This username is already taken";
 		}else if (userExist == 1) {
-			loginError = "The password is wrong partner";
+			registerError = "The passwords don't match partner";
 		}else if (userExist == 2) {
-			loginError = "Success";
 			forwardUrl = "/homePage.jsp";
 			session.setAttribute("connected", "true");
 			session.setAttribute("username", username);
+		}else if(userExist == 3) {
+			registerError = "No username put";
+		}else if(userExist == 4) {
+			registerError = "No password put";
 		}
 		else {
 			//Shouldn't be here, like the method only returns 0,1,or 2. So probably userExist never set
 		}
-		request.setAttribute("loginError", loginError);
+		request.setAttribute("registerError", registerError);
 		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(forwardUrl);
         dispatch.forward(request, response);
-		
 	}
 
 	/**
