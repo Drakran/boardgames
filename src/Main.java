349eec2fc313bf4acf4a8a95cf55787297fdb28d
Terry Wang <terrywangce@gmail.com>
2019-11-30 17:59:29 -0800

    trying heroku

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 import com.google.api.Page;
 import com.google.api.client.util.Lists;
@@ -10,7 +15,12 @@ import com.google.cloud.storage.StorageOptions;
 
 public class Main {
 	public static void main(String [] args) throws IOException {
-		System.out.println("Hi");
-		Storage storage = (Storage) StorageOptions.getDefaultInstance().getService();
+		 	Path gcp_key_file = Paths.get(System.getenv("GCP_KEY_FILE"));
+	        Files.deleteIfExists(gcp_key_file);
+	        Files.createFile(gcp_key_file);
+	        String env = System.getenv("GCP_CRED");
+	        Files.write(gcp_key_file, env.getBytes(), StandardOpenOption.WRITE);
+	       // Files.writeString(gp_keyc_file, System.getenv("GCP_CRED"), StandardOpenOption.WRITE);
+	        
 		}
 }
