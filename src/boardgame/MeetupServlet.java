17d084d109572795bb7b8296ad1ad6445f366f98
Terry Wang <terrywangce@gmail.com>
2019-11-30 23:53:37 -0800

    edited login to set user object in session and create meetup stuff

--- src/boardgame/MeetupServlet.java
+++ src/boardgame/MeetupServlet.java
@@ -2,11 +2,14 @@ package boardgame;
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Meetup
@@ -22,6 +25,30 @@ public class MeetupServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String createMeetupError = null;
+		HttpSession session = request.getSession();
+		System.out.println("print1: "+request.getSession().getId());
+		String forwardUrl = "/homepage.jsp";
+		String username = (String) session.getAttribute("username");
+		accessData access = new accessData();
+		if(username == null || username == "") {
+			
+		}
+		else {
+			
+		}
+		
+	
+		
+		
+		request.setAttribute("createMeetupError", createMeetupError);
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(forwardUrl);
+        dispatch.forward(request, response);
+		
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
